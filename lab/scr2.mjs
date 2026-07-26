1795b17004b5dbbdab68ba07847e5ac6e4171653
Mike Bannister <notimpossiblemike@gmail.com>
2026-07-26 00:35:35 -0400

    lab: drop a scratch file committed by mistake

--- lab/scr2.mjs
+++
@@ -1,9 +0,0 @@
-import { startOrigin, ab, withSession, AB_BIN } from './lib.mjs';
-const origin = await startOrigin();
-console.log('binary under test:', AB_BIN);
-await withSession('scrn', async (s) => {
-  await ab(['--session', s, 'open', `${origin.base}/screen.html`], { timeoutMs: 120000 });
-  for (let i=0;i<40 && origin.collected.length===0;i++) await new Promise(r=>setTimeout(r,250));
-});
-await origin.close();
-console.log(JSON.stringify(origin.collected[0]));
